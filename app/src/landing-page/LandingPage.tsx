24597d811a31546d1625309ef83dd9bddf8572b3
Matija Sosic <matija.sosic@gmail.com>
2025-07-04 17:56:49 +0200

    Removed all sections but hero from the LP.

--- app/src/landing-page/LandingPage.tsx
+++ app/src/landing-page/LandingPage.tsx
@@ -11,12 +11,7 @@ export default function LandingPage() {
     <div className='bg-white dark:text-white dark:bg-boxdark-2'>
       <main className='isolate dark:bg-boxdark-2'>
         <Hero />
-        <Clients />
-        <Features features={features} />
-        <Testimonials testimonials={testimonials} />
-        <FAQ faqs={faqs} />
       </main>
-      <Footer footerNavigation={footerNavigation} />
     </div>
   );
 }
